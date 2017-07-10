465736bd972fc6d5d7aa69d69ccedd066c5bbe17
Jeff Minard <jeff@jrm.cc>
2017-07-09 20:03:50 -0700

    Status checks record logs and make them available at new REST endpoint

--- main.tf
+++ main.tf
@@ -25,9 +25,32 @@ data "archive_file" "lambda" {
   output_path = "lambda.zip"
 }
 
+// we use this same deployment for several functions, lets upload to s3 instead of multiple uploads
+resource "aws_s3_bucket" "tf_gh_check_bucket" {
+  bucket = "tf-gh-check"
+}
+
+resource "aws_s3_bucket_object" "tf_gh_check_bucket_lambdazip" {
+  key    = "tf_gh_check_bucket_lambda.zip"
+  bucket = "${aws_s3_bucket.tf_gh_check_bucket.bucket}"
+  source = "${data.archive_file.lambda.output_path}"
+  etag   = "${data.archive_file.lambda.output_md5}"
+}
+
+// overall gateway
+resource "aws_api_gateway_rest_api" "tf_gh_check_gateway_api" {
+  name = "tf_gh_check-api"
+  description = "Gateway for terraformed gh check"
+}
+
+
+// Primary interface; incoming lambda function
+
 resource "aws_lambda_function" "tf_gh_check_function_incoming" {
-  filename = "${data.archive_file.lambda.output_path}"
-  function_name = "tf_gh_check"
+  s3_bucket         = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.bucket}"
+  s3_key            = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.key}"
+  s3_object_version = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.version_id}"
+  function_name = "tf_gh_check_function_incoming"
   role = "${aws_iam_role.tf_gh_check_role.arn}"
   handler = "main.incoming"
   runtime = "python2.7"
@@ -36,28 +59,12 @@ resource "aws_lambda_function" "tf_gh_check_function_incoming" {
   timeout = 5
 }
 
-resource "aws_lambda_function" "tf_gh_check_function_secondary" {
-  filename = "${data.archive_file.lambda.output_path}"
-  function_name = "tf_gh_check_function_secondary"
-  role = "${aws_iam_role.tf_gh_check_role.arn}"
-  handler = "main.secondary"
-  runtime = "python2.7"
-  source_code_hash = "${data.archive_file.lambda.output_base64sha256}"
-  publish = true
-  timeout = 30
-}
-
 resource "aws_lambda_permission" "allow_api_gateway" {
   function_name = "${aws_lambda_function.tf_gh_check_function_incoming.arn}"
   statement_id = "AllowExecutionFromApiGateway"
   action = "lambda:InvokeFunction"
   principal = "apigateway.amazonaws.com"
   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.tf_gh_check_gateway_api.id}/*/${aws_api_gateway_method.tf_gh_check_gateway_method.http_method}${aws_api_gateway_resource.tf_gh_check_gateway_resource.path}"
-}
-
-resource "aws_api_gateway_rest_api" "tf_gh_check_gateway_api" {
-  name = "tf_gh_check-api"
-  description = "Gateway for terraformed gh check"
 }
 
 resource "aws_api_gateway_resource" "tf_gh_check_gateway_resource" {
@@ -86,15 +93,91 @@ resource "aws_cloudwatch_log_group" "tf_gh_check_logs" {
   name = "/aws/lambda/${aws_lambda_function.tf_gh_check_function_incoming.function_name}"
 }
 
+
+// Secondary function, no api gateway just direct invocation from the first
+
+resource "aws_lambda_function" "tf_gh_check_function_secondary" {
+  s3_bucket         = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.bucket}"
+  s3_key            = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.key}"
+  s3_object_version = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.version_id}"
+  function_name = "tf_gh_check_function_secondary"
+  role = "${aws_iam_role.tf_gh_check_role.arn}"
+  handler = "main.secondary"
+  runtime = "python2.7"
+  source_code_hash = "${data.archive_file.lambda.output_base64sha256}"
+  publish = true
+  timeout = 30
+}
+
+
+// Log viewing function and api gateway configurations
+
+resource "aws_lambda_function" "tf_gh_check_function_logview" {
+  s3_bucket         = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.bucket}"
+  s3_key            = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.key}"
+  s3_object_version = "${aws_s3_bucket_object.tf_gh_check_bucket_lambdazip.version_id}"
+  function_name = "tf_gh_check_function_logview"
+  role = "${aws_iam_role.tf_gh_check_role.arn}"
+  handler = "main.logview"
+  runtime = "python2.7"
+  source_code_hash = "${data.archive_file.lambda.output_base64sha256}"
+  publish = true
+  timeout = 10
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_log" {
+  function_name = "${aws_lambda_function.tf_gh_check_function_logview.arn}"
+  statement_id = "AllowExecutionFromApiGateway"
+  action = "lambda:InvokeFunction"
+  principal = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.tf_gh_check_gateway_api.id}/*/${aws_api_gateway_method.tf_gh_check_gateway_method_log.http_method}${aws_api_gateway_resource.tf_gh_check_gateway_resource_log.path}"
+}
+
+resource "aws_api_gateway_resource" "tf_gh_check_gateway_resource_log" {
+  rest_api_id = "${aws_api_gateway_rest_api.tf_gh_check_gateway_api.id}"
+  parent_id = "${aws_api_gateway_rest_api.tf_gh_check_gateway_api.root_resource_id}"
+  path_part = "log"
+}
+
+resource "aws_api_gateway_method" "tf_gh_check_gateway_method_log" {
+  rest_api_id = "${aws_api_gateway_rest_api.tf_gh_check_gateway_api.id}"
+  resource_id = "${aws_api_gateway_resource.tf_gh_check_gateway_resource_log.id}"
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "tf_gh_check_integration_log" {
+  rest_api_id = "${aws_api_gateway_rest_api.tf_gh_check_gateway_api.id}"
+  resource_id = "${aws_api_gateway_resource.tf_gh_check_gateway_resource_log.id}"
+  http_method = "${aws_api_gateway_method.tf_gh_check_gateway_method_log.http_method}"
+  type = "AWS_PROXY"
+  uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${aws_lambda_function.tf_gh_check_function_logview.function_name}/invocations"
+  integration_http_method = "POST"
+}
+
+resource "aws_cloudwatch_log_group" "tf_gh_logview" {
+  name = "/aws/lambda/${aws_lambda_function.tf_gh_check_function_logview.function_name}"
+}
+
+
+
+
+
 resource "aws_api_gateway_deployment" "tf_gh_check_prod" {
   depends_on = [
     "aws_api_gateway_method.tf_gh_check_gateway_method",
-    "aws_api_gateway_integration.tf_gh_check_integration"
+    "aws_api_gateway_integration.tf_gh_check_integration",
+    "aws_api_gateway_method.tf_gh_check_gateway_method_log",
+    "aws_api_gateway_integration.tf_gh_check_integration_log",
   ]
   rest_api_id = "${aws_api_gateway_rest_api.tf_gh_check_gateway_api.id}"
   stage_name = "prod"
 }
 
-output "prod_url" {
+
+output "webhook_url" {
   value = "https://${aws_api_gateway_deployment.tf_gh_check_prod.rest_api_id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.tf_gh_check_prod.stage_name}${aws_api_gateway_resource.tf_gh_check_gateway_resource.path}"
+}
+output "log_url" {
+  value = "https://${aws_api_gateway_deployment.tf_gh_check_prod.rest_api_id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.tf_gh_check_prod.stage_name}${aws_api_gateway_resource.tf_gh_check_gateway_resource_log.path}"
 }
--- src/main.py
+++ src/main.py
@@ -1,20 +1,87 @@
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(__file__), "vendor"))
 
-import json, boto3, hmac, hashlib, yaml, github
+import json, boto3, hmac, hashlib, yaml, github, pyDes, base64, urllib, logging, datetime
 from pykwalify.core import Core
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-def invoke_secondary(hookname, hookconfig, originalbody):
+
+def invoke_secondary(hookname, hookconfig, original_event):
     client = boto3.client('lambda')
-    originalbody['_secondary'] = hookname
-    originalbody['_hookconfig'] = hookconfig
-    print "Sending secondary call {} with config {}".format(hookname, json.dumps(hookconfig))
-    print client.invoke(
+    original_event['_secondary'] = hookname
+    original_event['_hookconfig'] = hookconfig
+    logger.info("Sending secondary call {} with config {}".format(hookname, json.dumps(hookconfig)))
+    logger.info(client.invoke(
         FunctionName='tf_gh_check_function_secondary',
         InvocationType='Event',
-        Payload=json.dumps(originalbody)
+        Payload=json.dumps(original_event)
+    ))
+
+
+def short_token():
+    with open(os.path.join(os.path.dirname(__file__), "ghtoken.txt")) as f:
+        ghtoken = f.read().strip()[0:24]
+    return ghtoken
+
+
+def encode_log_location(group, stream, reqid):
+    val = json.dumps([group, stream, reqid])
+    enc = pyDes.triple_des(short_token()).encrypt(val, padmode=pyDes.PAD_PKCS5)
+    return base64.encodestring(enc)
+
+
+def decode_log_location(base64id):
+    dec = base64.decodestring(base64id)
+    unencrypted = pyDes.triple_des(short_token()).decrypt(dec, padmode=pyDes.PAD_PKCS5)
+    inflated = json.loads(unencrypted)
+    return inflated
+
+
+def logview(event, context):
+    print json.dumps(event) # not logger.info() so it doesn't show up in logview itself :)
+
+    qsp = event.get('queryStringParameters')
+    if not qsp:
+        return {"body": 'Missing logset id (qsp)', "statusCode": 404, "headers": {"Content-Type": "text/html"}}
+
+    lsid = qsp.get('logsetid', False)
+    if not lsid:
+        return {"body": 'Missing logset id (missing)', "statusCode": 404, "headers": {"Content-Type": "text/html"}}
+
+    group, stream, rid = decode_log_location(lsid)
+    filterpattern = '"{}"'.format(rid)
+
+    logger.info("Fetching log for {} : {} : {} (Pattern: {})".format(group, stream, rid, filterpattern))
+
+    # todo: try/catch for no longer existant logs
+    client = boto3.client('logs')
+    r = client.filter_log_events(
+        logGroupName=group,
+        logStreamNames=[stream],
+        filterPattern=filterpattern
     )
+    out = '<html>' \
+          '<head>' \
+          '<meta name="referrer" content="always">' \
+          '<title>Log</title>' \
+          '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"' \
+          ' rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"' \
+          ' crossorigin="anonymous">' \
+          '</head>' \
+          '<body>' \
+          '<table class="table table-striped table-hover table-condensed">' \
+          '<thead class="thead-inverse"><tr><th>Timstamp</th><th>Message</th></tr></thead>' \
+          '<tbody>'
+    for event in r.get('events', []):
+        out += "<tr><td nowrap><a href='#{}'>{}</a></td><td>{}</td></tr>".format(
+            event.get('eventId'),
+            datetime.datetime.utcfromtimestamp(event.get('timestamp') / 1000).strftime('%Y-%m-%d %H:%M:%S'),
+            escape_html(event.get('message').strip())
+        )
+    out += "</tbody></table></body></html>"
+    return {"body": out, "statusCode": 200, "headers": {"Content-Type": "text/html"}}
 
 
 def incoming(event, context):
@@ -22,7 +89,7 @@ def incoming(event, context):
     Validate the incoming event from the API gateway
     """
 
-    print json.dumps(event)
+    print json.dumps(event)  # not logger.info() so it doesn't show up in logview itself :)
 
     # validate the secret
     if not validate_secret(event['headers'].get('X-Hub-Signature'), event['body']):
@@ -34,7 +101,7 @@ def incoming(event, context):
 
     # this will only work, for now, with hooks that include repo information
     if 'repository' not in hookdata:
-        print "No repository in the hook, no processing"
+        logger.error("No repository in the hook, no processing")
         return {"body": json.dumps({"error": "unsupported hook type; missing repository information"}), "statusCode": 501}
 
     repo = hookdata['repository']['full_name']
@@ -42,9 +109,9 @@ def incoming(event, context):
     # Now, we fetch the config from the repo to see what hooks we should trigger
     try:
         hooks_yml = get_github().get_repo(repo, lazy=True).get_file_contents('.hooks.yml')
-        print "Fetched .hooks.yml from repo {}".format(repo)
+        logger.info("Fetched .hooks.yml from repo {}".format(repo))
     except github.GithubException:
-        print "Missig .hooks.yml on repo {}".format(repo)
+        logger.error("Missig .hooks.yml on repo {}".format(repo))
         return {"body": json.dumps({"error": "no .hooks.yml present"}), "statusCode": 501}
 
     # todo: safe load try/catch needed here
@@ -54,7 +121,7 @@ def incoming(event, context):
     c = Core(source_data=hook_config, schema_files=[os.path.join(os.path.dirname(__file__), "hooks.schema.yml")])
     c.validate(raise_exception=False)
     if len(c.validation_errors) > 0:
-        print c.validation_errors
+        logger.error(c.validation_errors)
         return {"body": json.dumps({"error": "invalid hooks configuration"}), "statusCode": 501}
 
     ghevent = event['headers'].get('X-GitHub-Event', '')
@@ -64,32 +131,32 @@ def incoming(event, context):
     # we _always_ run the .hooks.yml schema validation check
     if (ghevent == 'pull_request' and
         'action' in hookdata and hookdata["action"] in ["opened", "synchronize", "reopened"]):
-        invoke_secondary('hooks_schema', {}, hookdata)
+        invoke_secondary('hooks_schema', {}, event)
 
     # yml validation is only on pull requests (re)open|sync
     if ('yml_validation' in hook_config['hooks'] and
         ghevent == 'pull_request' and
         'action' in hookdata and hookdata["action"] in ["opened", "synchronize", "reopened"]
        ):
-        invoke_secondary('yml_validation', hook_config['hooks']['yml_validation'], hookdata)
+        invoke_secondary('yml_validation', hook_config['hooks']['yml_validation'], event)
 
     # A silly one that always passes on pull requests (re)open|sync
     if ('always_pass' in hook_config['hooks'] and
         ghevent == 'pull_request' and
         'action' in hookdata and hookdata["action"] in ["opened", "synchronize", "reopened"]
        ):
-        invoke_secondary('always_pass', hook_config['hooks']['always_pass'], hookdata)
+        invoke_secondary('always_pass', hook_config['hooks']['always_pass'], event)
 
     # approval count triggers on pr (re)open as well as pr_review events
     if ('approval_count' in hook_config['hooks'] and (
          (ghevent == 'pull_request' and
-          'action' in hookdata and hookdata["action"] in ["opened", "reopened"])
+          'action' in hookdata and hookdata["action"] in ["opened", "synchronize", "reopened"])
          or
          (ghevent == 'pull_request_review' and
           'action' in hookdata and hookdata["action"] in ["submitted", "edited"])
         )
        ):
-        invoke_secondary('approval_count', hook_config['hooks']['approval_count'], hookdata)
+        invoke_secondary('approval_count', hook_config['hooks']['approval_count'], event)
 
     # all done!
     return {"body": json.dumps({"message": "Thanks"}), "statusCode": 200}
@@ -99,78 +166,84 @@ def secondary(event, context):
     """
     Evaluate a secondary call
     """
-    print json.dumps(event)
-    print "Processing secondary call..."
+    print json.dumps(event)  # not logger.info() so it doesn't show up in logview itself :)
+
+    logger.info("Processing secondary call...")
 
     if '_secondary' not in event: # wtf
         return
 
-    print "Secondary call is {} with config {}".format(event['_secondary'], json.dumps(event['_hookconfig']))
+    es = event['_secondary']
+    logger.info("Secondary call is {} with config {}".format(event['_secondary'], json.dumps(event['_hookconfig'])))
 
-    if event['_secondary'] == 'hooks_schema':
-        hooks_schema(event)
-    elif event['_secondary'] == 'yml_validation':
-        yml_validation(event)
-    elif event['_secondary'] == 'approval_count':
-        approval_count(event)
-    elif event['_secondary'] == 'always_pass':
-        always_pass(event)
+    if es == 'hooks_schema':
+        hooks_schema(event, context)
+    elif es == 'yml_validation':
+        yml_validation(event, context)
+    elif es == 'approval_count':
+        approval_count(event, context)
+    elif es == 'always_pass':
+        always_pass(event, context)
     else:
         # huh
-        print "Unsupported method"
+        logger.error("Unsupported method: ".format(es))
 
-    print "Secondary call for {} complete".format(event['_secondary'])
+    logger.info("Secondary call for {} complete".format(es))
 
 
-def always_pass(event):
+def always_pass(event, context):
     """
     Add a passing status check to a pr
     """
-    repofullname = event['repository']['full_name']
-    commitsha = event['pull_request']['head']['sha']
-    get_github()\
-        .get_repo(repofullname)\
-        .get_commit(commitsha)\
-        .create_status('success', description="You're awesome!", context="always_pass")
+    fake_done('always_pass', event, context)
 
 
-def approval_count(event):
+def approval_count(event, context):
     """
     Add a pass if the # of approvers is >= config value
     """
     # todo: actually imlement
-    repofullname = event['repository']['full_name']
-    commitsha = event['pull_request']['head']['sha']
-    get_github()\
-        .get_repo(repofullname)\
-        .get_commit(commitsha)\
-        .create_status('success', description="Approvals all found", context="approval_count")
+    fake_done('approval_count', event, context)
 
 
-def yml_validation(event):
+def yml_validation(event, context):
     """
     Scan all specified paths and confirm that the yml files are loadably safe
     """
     # todo: actually imlement
-    repofullname = event['repository']['full_name']
-    commitsha = event['pull_request']['head']['sha']
-    get_github()\
-        .get_repo(repofullname)\
-        .get_commit(commitsha)\
-        .create_status('success', description="All yml files are valid", context="yml_validation")
+    fake_done('yml_validation', event, context)
 
 
-def hooks_schema(event):
+def hooks_schema(event, context):
     """
     Check if the PR has any changed to .hooks.yml and validate the file
     """
     # todo: actually imlement
-    repofullname = event['repository']['full_name']
-    commitsha = event['pull_request']['head']['sha']
+    logger.info("Fetching changelist")
+    logger.info("Seeing if .hooks.yml present")
+    logger.info("Validating branch .hooks.yml")
+    fake_done('hooks_schema', event, context)
+
+
+def fake_done(name, event, context):
+    ghevent = json.loads(event['body'])
+    repofullname = ghevent['repository']['full_name']
+    commitsha = ghevent['pull_request']['head']['sha']
+    lsid = encode_log_location(context.log_group_name, context.log_stream_name, context.aws_request_id)
+    url = 'https://{}/prod/log?{}'.format(event['headers']['Host'], urllib.urlencode({'logsetid': lsid}))
+    logger.info("Sending success to repo {} on sha {} for context {} with url {}".format(
+        repofullname,
+        commitsha,
+        name,
+        url
+    ))
     get_github()\
         .get_repo(repofullname)\
         .get_commit(commitsha)\
-        .create_status('success', description=".hooks.yml valid!", context="hooks_schema")
+        .create_status('success',
+                       description="{} completed".format(name),
+                       context=name,
+                       target_url=url)
 
 
 def get_github():
@@ -184,29 +257,41 @@ def validate_secret(header_signature, msg):
         secret = f.read().strip()
 
     if header_signature is None:
+        logger.error("Header signature missing")
         return False
 
     sha_name, signature = header_signature.split('=')
     if sha_name != 'sha1':
+        logger.error("Header signature not signed with sha1")
         return False
 
     # HMAC requires the key to be bytes, but data is string
     mac = hmac.new(str(secret), msg=msg, digestmod=hashlib.sha1)
 
+    # Get ours vs. theirs
+    expected = str(mac.hexdigest())
+    received = str(signature)
+
     # Timing attack secure comparison
-    return hmac.compare_digest(str(mac.hexdigest()), str(signature))
+    matches = hmac.compare_digest(expected, received)
+
+    if not matches:
+        logger.error("Header signature ({}) does not match expected ({})".format(received, expected))
+
+    return matches
+
+
+def escape_html(text):
+    """escape strings for display in HTML"""
+    import cgi
+    return cgi.escape(text, quote=True).\
+           replace(u'\n', u'<br />').\
+           replace(u'\t', u'&emsp;').\
+           replace(u'  ', u' &nbsp;')
 
 
 # def main():
-#     with open(os.path.join(os.path.dirname(__file__), "ghtoken.txt")) as f:
-#         ghtoken = f.read().strip()
-#     g = github.Github(ghtoken)
-#     try:
-#         hooks = g.get_repo('chuyskywalker/lambchops').get_file_contents('.hooks.ymlx')
-#         print hooks.decoded_content
-#     except github.GithubException:
-#         print "no file"
-#
+#     pass # stuff
 #
 # # For local testing
 # if __name__ == "__main__":
