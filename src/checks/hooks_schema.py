747b04e9dfbbc3e48c054895137e389f1860ebc1
Jeff Minard <jeff@jrm.cc>
2017-07-10 23:04:17 -0700

    Actual code for validating .hooks.yml files in PRs

--- src/checks/hooks_schema.py
+++ src/checks/hooks_schema.py
@@ -1,7 +1,12 @@
-from util.fake_done import fake_done
-import check, logging
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "vendor"))
 
-log = logging.getLogger(__name__)
+import check, logging, json, github, yaml
+from util.fake_done import send_status
+from util.tfgithub import get_github
+from pykwalify.core import Core
+
+logger = logging.getLogger(__name__)
 
 
 class HooksScehma(check.Check):
@@ -18,7 +23,37 @@ class HooksScehma(check.Check):
         return False
 
     def run(self, event, context):
-        log.info("Fetching changelist")
-        log.info("Seeing if .hooks.yml present")
-        log.info("Validating branch .hooks.yml")
-        fake_done(self.configname, event, context)
+
+        gh_hook = json.loads(event['body'])
+
+        logger.info("Fetching .hook.yml from branch")
+
+        repo = gh_hook['repository']['full_name']
+        sha = gh_hook['pull_request']['head']['sha']
+
+        try:
+            hooks_yml = get_github().get_repo(repo, lazy=True).get_file_contents('.hooks.yml', ref=sha)
+            logger.info("Fetched .hooks.yml from repo {}".format(repo))
+        except github.GithubException:
+            logger.error("Missig .hooks.yml on repo {}".format(repo))
+            send_status(event, context, gh_hook, self.configname, 'success', ".hooks.yml not present in branch")
+            return
+
+        try:
+            hook_config = yaml.safe_load(hooks_yml.decoded_content)
+            logger.info("Basic yml validation passed")
+        except Exception as e:
+            logger.error("Failed to decode hook yaml: " + e.message)
+            send_status(event, context, gh_hook, self.configname, 'failure', "Could not decode branch .hooks.yml")
+            return
+
+        logger.info("Advanced schema validation")
+        c = Core(source_data=hook_config,
+                 schema_files=[os.path.join(os.path.dirname(__file__), "..", "hooks.schema.yml")])
+        c.validate(raise_exception=False)
+        if len(c.validation_errors) > 0:
+            logger.error(c.validation_errors)
+            send_status(event, context, gh_hook, self.configname, 'failure', ".hooks.yml has validation errors; see log")
+            return
+
+        send_status(event, context, gh_hook, self.configname, 'success', ".hooks.yml present and valid")
--- src/util/fake_done.py
+++ src/util/fake_done.py
@@ -7,11 +7,16 @@ logger = logging.getLogger(__name__)
 
 def fake_done(name, event, context):
     ghevent = json.loads(event['body'])
+    send_status(event, context, ghevent, name, 'success', "{} completed".format(name))
+
+
+def send_status(event, context, ghevent, name, status, desc):
     repofullname = ghevent['repository']['full_name']
     commitsha = ghevent['pull_request']['head']['sha']
     lsid = encode_log_location(context.log_group_name, context.log_stream_name, context.aws_request_id)
     url = 'https://{}/prod/log?{}'.format(event['headers']['Host'], urllib.urlencode({'logsetid': lsid}))
-    logger.info("Sending success to repo {} on sha {} for context {} with url {}".format(
+    logger.info("Sending {} to repo {} on sha {} for context {} with url {}".format(
+        status,
         repofullname,
         commitsha,
         name,
@@ -20,7 +25,7 @@ def fake_done(name, event, context):
     get_github()\
         .get_repo(repofullname)\
         .get_commit(commitsha)\
-        .create_status('success',
-                       description="{} completed".format(name),
+        .create_status(status,
+                       description=desc,
                        context=name,
                        target_url=url)
